0cf61a38215e36255e1ef1fe0b5d312f560956f0
OK_Diamond <ok.thekirks@gmail.com>
2024-05-07 22:44:45 +0100

    Add Colours class to common.py, which requires the dataclasses module

--- src/zemia/common.py
+++ src/zemia/common.py
@@ -1,6 +1,20 @@
 '''
 Used for various common tasks.
 '''
+from dataclasses import dataclass
+
+@dataclass
+class Colours:
+    '''Holds colours for the terminal.'''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def empty(a: int|list|str) -> bool:
     '''
